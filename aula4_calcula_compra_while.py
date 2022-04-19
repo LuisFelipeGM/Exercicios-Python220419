a6c502eb010a29022c2f90b364da24dbb1ed2346
Logon Aluno <logonrmlocal@fiap.com.br>
2022-04-19 11:31:51 -0300

    fiz o Exercicio de compra com while

---
+++ aula4_calcula_compra_while.py
@@ -0,0 +1,13 @@
+prod = input("Deseja começar a calcular o valor da sua compra? ")
+total = 0
+indice = 1
+
+while prod == "s" or prod == "sim":
+    preco = int(input(f"Digite o valor do {indice}° produto: "))
+    indice += 1
+    total += preco 
+    prod = input("Tem mais produtos? (Digite -1 se não tiver mais produtos) ")
+    if prod == -1:
+        break
+
+print(f"O valor da compra foi de R${total}!")
