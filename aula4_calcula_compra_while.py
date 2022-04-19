f94365b595c2e8d63d9be13a61254a77163be41b
LuisFelipeGM <rm94051@fiap.com.br>
2022-04-19 11:33:17 -0300

    Sim

--- aula4_calcula_compra_while.py
+++ aula4_calcula_compra_while.py
@@ -3,7 +3,7 @@ total = 0
 indice = 1
 
 while prod == "s" or prod == "sim":
-    preco = int(input(f"Digite o valor do {indice}° produto: "))
+    preco = float(input(f"Digite o valor do {indice}° produto: "))
     indice += 1
     total += preco 
     prod = input("Tem mais produtos? (Digite -1 se não tiver mais produtos) ")
